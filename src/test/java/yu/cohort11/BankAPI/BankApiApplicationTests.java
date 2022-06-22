10151fdf8d58e83dfde7f07c53b38e447a6c9347
Kyle Simpson <kyleochate@gmail.com>
2022-06-22 12:35:54 -0400

    Initial commit

---
+++ src/test/java/yu/cohort11/BankAPI/BankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package yu.cohort11.BankAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
