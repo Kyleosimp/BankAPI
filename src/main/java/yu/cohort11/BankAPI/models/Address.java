ed581f7e169bf45d3f895c839bd19b38e5f71231
Amundy98 <aisha.mundy@gmail.com>
2022-06-23 12:11:19 -0400

    address pojo

---
+++ src/main/java/yu/cohort11/BankAPI/models/Address.java
@@ -0,0 +1,95 @@
+package yu.cohort11.BankAPI.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Address {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String street_name;
+
+    private String street_number;
+
+    private String city;
+
+    private String state;
+
+    private String zip;
+
+    public Address() {
+    }
+
+    public Address(Long id, String street_name, String street_number, String city, String state, String zip) {
+        this.id = id;
+        this.street_name = street_name;
+        this.street_number = street_number;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getStreet_name() {
+        return street_name;
+    }
+
+    public void setStreet_name(String street_name) {
+        this.street_name = street_name;
+    }
+
+    public String getStreet_number() {
+        return street_number;
+    }
+
+    public void setStreet_number(String street_number) {
+        this.street_number = street_number;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    @Override
+    public String toString() {
+        return "Address{" +
+                "id=" + id +
+                ", street_name='" + street_name + '\'' +
+                ", street_number='" + street_number + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zip='" + zip + '\'' +
+                '}';
+    }
+}
