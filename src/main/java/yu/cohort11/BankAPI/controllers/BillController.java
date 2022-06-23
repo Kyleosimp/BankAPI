9d82c017279be265d5c588313f7f8de674e757fb
Kyle Simpson <kyleochate@gmail.com>
2022-06-22 21:51:00 -0400

    "Bills basic CRUD functionality"

--- src/main/java/yu/cohort11/BankAPI/controllers/BillController.java
+++ src/main/java/yu/cohort11/BankAPI/controllers/BillController.java
@@ -1,4 +1,46 @@
 package yu.cohort11.BankAPI.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import yu.cohort11.BankAPI.BankApiApplication;
+import yu.cohort11.BankAPI.models.Bill;
+import yu.cohort11.BankAPI.repositories.BillRepository;
+import yu.cohort11.BankAPI.services.BillService;
+
+import java.util.List;
+
+@RestController
 public class BillController {
+
+    private static final Logger logger = LoggerFactory.getLogger(BankApiApplication.class);
+
+    @Autowired
+    private BillService billService;
+    private BillRepository billRepository;
+
+    @PostMapping("/bill")
+    public Bill createBill(Bill bill){
+        return billService.createBill(bill);
+    }
+
+    @GetMapping("/bill/{id}")
+    public Bill getBillById(@PathVariable Long id){
+        return billService.getBillById(id);
+    }
+
+    @GetMapping("/bill")
+    public List<Bill> getAllBills(){
+        return billService.getAllBills();
+    }
+
+    @PutMapping("/bill/{id}")
+    public Bill updateBill(Long id, Bill bill){
+        return billService.updateBill(id, bill);
+    }
+    @DeleteMapping("/bill/{id}")
+    public void deleteBillById(Long id){
+        billService.deleteBillById(id);
+    }
 }
--- src/main/java/yu/cohort11/BankAPI/repositories/BillRepository.java
+++ src/main/java/yu/cohort11/BankAPI/repositories/BillRepository.java
@@ -1,4 +1,10 @@
 package yu.cohort11.BankAPI.repositories;
 
-public interface BillRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import yu.cohort11.BankAPI.models.Bill;
+
+@Repository
+public interface BillRepository extends CrudRepository<Bill, Long> {
 }
