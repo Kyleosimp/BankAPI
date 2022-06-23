aab6ef0ab62a21cb2fe43d1dc8806116416354f3
myhomeworkfone <adzadurenne@gmail.com>
2022-06-23 12:12:35 -0400

    my first commit

---
+++ src/main/java/yu/cohort11/BankAPI/controllers/CustomerController.java
@@ -0,0 +1,72 @@
+package yu.cohort11.BankAPI.controllers;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import yu.cohort11.BankAPI.BankApiApplication;
+import yu.cohort11.BankAPI.models.Customer;
+import yu.cohort11.BankAPI.repositories.CustomerRepository;
+import yu.cohort11.BankAPI.services.CustomerService;
+
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
+
+public class CustomerController {
+
+    private static final Logger logger = LoggerFactory.getLogger(BankApiApplication.class);
+
+    @Autowired
+    private CustomerService customerService;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+
+    @PostMapping("/Customers")
+    public ResponseEntity<?> addCustomer(@Validated @RequestBody Customer customer){
+        logger.info("Successfully ADDED an order with name :" + customer);
+        customerService.saveCustomer(customer);
+        HttpHeaders responseHeaders = new HttpHeaders();
+        URI newPollUri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(customer.getId()).toUri();
+        responseHeaders.setLocation(newPollUri);
+        return new ResponseEntity<>(null, responseHeaders, HttpStatus.CREATED);
+    }
+
+
+    @PutMapping("/customer/{id}")
+    public ResponseEntity<Optional<Customer>> updateCustomer(@PathVariable Long id, @RequestBody Customer customer){
+        logger.info("updating customer order with id :" + id);
+        return new ResponseEntity (this.customerService.updateCustomer(id,customer), HttpStatus.ACCEPTED);
+    }
+
+    @GetMapping("/customers/{id}")
+    public ResponseEntity<Optional<Customer>> getCustomerById(@PathVariable Long id) {
+        logger.info("successfully retrieved order with id :" + id);
+        return new ResponseEntity<>(this.customerService.getCustomerById(id), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/customers/{id}")
+    public  ResponseEntity<?> deleteCustomerById(@PathVariable Long id){
+        logger.info("successfully DELETED an order from list with id: " +id);
+        customerService.deleteCustomerById(id);
+        return new ResponseEntity<>(HttpStatus.OK);
+
+    }
+
+    @GetMapping("/customers")
+    public ResponseEntity<List<Customer>> getAllCustomer(@RequestParam(required = false) Long id){
+        if(id != null){
+            return new ResponseEntity<>(customerRepository.findCustomerById(id),HttpStatus.OK);
+        }
+        logger.info("Successfully retrieved all customer order " );
+
+        return new ResponseEntity<>( customerService.getAllCustomer(),HttpStatus.OK);
+    }
+}
---
+++ src/main/java/yu/cohort11/BankAPI/services/CustomerService.java
@@ -0,0 +1,46 @@
+package yu.cohort11.BankAPI.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import yu.cohort11.BankAPI.models.Customer;
+import yu.cohort11.BankAPI.repositories.CustomerRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CustomerService {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    public void saveCustomer(Customer customer) {
+        customerRepository.save(customer);
+    }
+
+    public List<Customer> getAllCustomer() {
+        List<Customer> customers = new ArrayList<>();
+        customerRepository.findAll().forEach(customers::add);
+        return customers;
+    }
+
+    public Customer updateCustomer(Long customerId, Customer customer) {
+        for (Customer c : getAllCustomer()) {
+            if (c.getId() == customerId) {
+                customerRepository.save(customer);
+            }
+        }
+        return customer;
+    }
+
+    public Optional<Customer> getCustomerById(Long id){
+        return customerRepository.findById(id);
+    }
+
+    public  void  deleteCustomerById(Long id){
+        customerRepository.deleteById(id);
+    }
+
+
+}
