81b9f3f4545e83963e367dbd78c0a05fa60bd91a
Kyleosimp <kyleochate@gmail.com>
2022-06-24 21:15:00 -0400

    "Updated Bill POJO, Controller and Services to match account"

--- src/main/java/yu/cohort11/BankAPI/models/Bill.java
+++ src/main/java/yu/cohort11/BankAPI/models/Bill.java
@@ -17,12 +17,31 @@ public class Bill {
     private Integer recurring_date;
     private String upcoming_payment_date;
     private Double payment_amount;
-    private String account_id;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Account account;
+
+    @ManyToOne(cascade = CascadeType.ALL)
+    private Customer customer;
+
 
     public Bill() {
     }
 
-    public Bill(Long id, String status, String payee, String nickname, String creation_date, String payment_date, Integer recurring_date, String upcoming_payment_date, Double payment_amount, String account_id) {
+//    public Bill(Long id, String status, String payee, String nickname, String creation_date, String payment_date, Integer recurring_date, String upcoming_payment_date, Double payment_amount, String account_id) {
+//        this.id = id;
+//        this.status = status;
+//        this.payee = payee;
+//        this.nickname = nickname;
+//        this.creation_date = creation_date;
+//        this.payment_date = payment_date;
+//        this.recurring_date = recurring_date;
+//        this.upcoming_payment_date = upcoming_payment_date;
+//        this.payment_amount = payment_amount;
+//        this.account_id = account_id;
+//    }
+
+public Bill(Long id, String status, String payee, String nickname, String creation_date, String payment_date, Integer recurring_date, String upcoming_payment_date, Double payment_amount, Account account, Customer customer) {
         this.id = id;
         this.status = status;
         this.payee = payee;
@@ -32,7 +51,13 @@ public class Bill {
         this.recurring_date = recurring_date;
         this.upcoming_payment_date = upcoming_payment_date;
         this.payment_amount = payment_amount;
-        this.account_id = account_id;
+        this.account = account;
+        this.customer = customer;
+    }
+
+
+    public void setCustomer(Customer customer) {
+        this.customer = customer;
     }
 
     public String getStatus() {
@@ -99,13 +124,20 @@ public class Bill {
         this.payment_amount = payment_amount;
     }
 
-    public String getAccount_id() {
-        return account_id;
+    public Account getAccount() {
+        return account;
     }
 
-    public void setAccount_id(String account_id) {
-        this.account_id = account_id;
+    public void setAccount(Account account) {
+        this.account = account;
     }
+    //    public String getAccount_id() {
+//        return account_id;
+//    }
+//
+//    public void setAccount_id(String account_id) {
+//        this.account_id = account_id;
+//    }
 
     public Long getId() {
         return id;
@@ -113,6 +145,10 @@ public class Bill {
 
     public void setId(Long id) {
         this.id = id;
+    }
+
+    public Customer getCustomer() {
+        return customer;
     }
 
     @Override
@@ -127,7 +163,8 @@ public class Bill {
                 ", recurring_date=" + recurring_date +
                 ", upcoming_payment_date='" + upcoming_payment_date + '\'' +
                 ", payment_amount=" + payment_amount +
-                ", account_id='" + account_id + '\'' +
+                ", account=" + account +
+                ", customer=" + customer +
                 '}';
     }
 }
